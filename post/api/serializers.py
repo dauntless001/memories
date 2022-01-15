06b80ee6f8cf246107ae1aa16131bdcb78d671dc
dauntless <suntopmultimedia@gmail.com>
2022-01-15 13:22:55 +0100

    exclude slug

--- post/api/serializers.py
+++ post/api/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        exclude = ['slug']
